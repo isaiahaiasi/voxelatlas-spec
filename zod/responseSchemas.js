da121cd7e2d0052c91c4df8d8e00e0255271154c
Isaiah A <isaiah.althouse@gmail.com>
2022-07-19 22:44:06 -0700

    Add zod schemas for successful Responses

---
+++ zod/responseSchemas.js
@@ -0,0 +1,56 @@
+const { z } = require('zod');
+const { Comment, Friendship, Like, Room, PaginationLinks } = require('./schemas');
+
+const successfulResponseSchemas = {
+  createComment: Comment,
+  createFriendship: Friendship,
+  createLike: Like,
+  createRoom: Room,
+  createUser: User,
+
+  deleteComment: Comment,
+  deleteFriendship: Friendship,
+  deleteLike: Like,
+  deleteRoom: Room,
+  deleteUser: User,
+
+  getCommentsByRoomId: z.object({
+    data: z.array(Comment),
+    links: PaginationLinks,
+  }),
+
+  getFriendships: z.object({
+    data: z.array(Friendship),
+    links: PaginationLinks,
+  }),
+
+  getLikesByRoomId: z.object({
+    data: z.array(Friendship),
+    links: PaginationLinks,
+  }),
+
+  getRoomById: Room,
+
+  getRooms: z.object({
+    data: z.array(Friendship),
+    links: PaginationLinks,
+  }),
+
+  getRoomsByUserId: z.object({
+    data: z.array(Room),
+    links: PaginationLinks,
+  }),
+
+  getUserById: User,
+
+  getUsers: z.object({
+    data: z.array(Friendship),
+    links: PaginationLinks,
+  }),
+
+  updateFriendship: Friendship,
+  updateRoom: Room,
+  updateUser: User,
+}
+
+module.exports = successfulResponseSchemas;
